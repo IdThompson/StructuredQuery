0f69759be10874832f7b449aad0d6491f6378d3b
Idowu Thompson <idowu.thompson@decagon.dev>
2023-07-27 09:23:55 +0100

    Structured_query started

---
+++ src/main/java/com/activEgde/structured_query/StructuredQueryApplication.java
@@ -0,0 +1,13 @@
+package com.activEgde.structured_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructuredQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activEgde/structured_query/StructuredQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activEgde.structured_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
